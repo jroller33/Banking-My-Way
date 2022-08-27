24e2eb8ff0356068dc98367df23e64f1bd045053
John Roller <jtroller33@gmail.com>
2022-08-27 09:04:22 -0500

    controller

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const api = require('./api/index');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(api);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+});
