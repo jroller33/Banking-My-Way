16592c32b4d354bab5917d5576007457cee82d30
John Roller <jtroller33@gmail.com>
2022-08-30 18:21:56 -0500

    server.js HTML routes

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const api = require('./controller/index');
 const sequelize = require('./config/connection');
+// const db = require("../models");
+
+// const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,6 +12,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(api);
+
+
+app.get("/", function(req, res) {
+  if (req.user) {
+      console.log(req.user + " user is logged in");
+      res.redirect("/members");
+  }
+  res.render("signup");
+});
+
+app.get("/login", function(req, res) {
+    if (req.user) {
+        res.redirect("/members");
+    }
+    res.render("login");
+});
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
