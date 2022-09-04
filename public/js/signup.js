0ed304cae58c74a71f7e992f9af9155163c81ad9
John Roller <jtroller33@gmail.com>
2022-09-03 22:33:43 -0500

    frontend JS done for POST /api/users. need to do backend.

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,41 +1,68 @@
-$(document).ready(function() {
-    var signUpForm = $("form.signup");
-    var emailInput = $("input#email-input");
-    var passwordInput = $("input#password-input");
+const signupFormHandler = async (event) => {
+    event.preventDefault();
 
-    signUpForm.on("submit", function(event) {
-        event.preventDefault();
-        var userData = {
-            email: emailInput.val().trim(),
-            password: passwordInput.val().trim()
-        };
+    const email = document.querySelector('#email-input').value.trim();
+    const password = document.querySelector('#password-input').value.trim();
 
-        if (!userData.email || !userData.password) {
-            return;
+    if (email && password) {
+        const response = await fetch('/api/users/', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            console.log('POST /api/users response.ok');
+            // document.location.replace('/login')
+        } else {
+            alert(response.statusText);
         }
-
-        signUpUser(userData.email, userData.password);
-        emailInput.val("");
-        passwordInput.val("");
-    });
-
-    function signUpUser(email, password) {
-        $.post("/api/signup", {                 // POST /api/signup
-            email: email,
-            password: password
-        })
-        .then(function() {
-            window.location.replace("/members");            // what is this?
-        })
-        .catch(handleLoginErr);
     }
+};
 
-    function handleLoginErr(err) {
-        if (err.responseJSON.errors[0].message === "users.email must be unique") {
-            const errTxt = "Email already in use";
-            $("#alert .msg").text(errTxt);
-        }
-        $("#alert").fadeIn(500);
-        console.log(err.responseJSON);
-    }
-});
+document
+    .querySelector('.signup')
+    .addEventListener('submit', signupFormHandler);
+
+
+// $(document).ready(function() {
+//     var signUpForm = $("form.signup");
+//     var emailInput = $("input#email-input");
+//     var passwordInput = $("input#password-input");
+
+//     signUpForm.on("submit", function(event) {
+//         event.preventDefault();
+//         var userData = {
+//             email: emailInput.val().trim(),
+//             password: passwordInput.val().trim()
+//         };
+
+//         if (!userData.email || !userData.password) {
+//             return;
+//         }
+
+//         signUpUser(userData.email, userData.password);
+//         emailInput.val("");
+//         passwordInput.val("");
+//     });
+
+//     function signUpUser(email, password) {
+//         $.post("/api/signup", {                 // POST /api/signup
+//             email: email,
+//             password: password
+//         })
+//         .then(function() {
+//             window.location.replace("/members");            // what is this?
+//         })
+//         .catch(handleLoginErr);
+//     }
+
+//     function handleLoginErr(err) {
+//         if (err.responseJSON.errors[0].message === "users.email must be unique") {
+//             const errTxt = "Email already in use";
+//             $("#alert .msg").text(errTxt);
+//         }
+//         $("#alert").fadeIn(500);
+//         console.log(err.responseJSON);
+//     }
+// });
